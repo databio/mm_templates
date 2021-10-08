130d5923fc959ad72c176519b837f5cecf417d9d
nsheff <nsheff@users.noreply.github.com>
2021-10-08 17:10:05 -0400

    index

--- index_contents.sh
+++ index_contents.sh
@@ -2,6 +2,7 @@ x=`find * -type f -name "*.jinja"`
 
 echo "{"
 for i in $x; do
-	echo "    \"${i%.jinja}\": \"$i\","
+  echo "    \"${i%.jinja}\": \"$i\","
 done
+echo "    \"destination\": \"markmeld\""
 echo "}"
